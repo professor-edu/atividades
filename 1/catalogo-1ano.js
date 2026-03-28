7e931739f22a4ea12f3449cf54dcf9b2260efb6c
professor-edu <jeduardosoeiro@gmail.com>
2026-03-28 22:10:57 +0000

    Update catalogo-1ano.js

--- 1/catalogo-1ano.js
+++ 1/catalogo-1ano.js
@@ -1,6 +1,6 @@
 // ======================================================================
 // CATÁLOGO CENTRAL DE JOGOS DO 1º ANO
-// Sempre que criares um jogo novo, basta adicioná-lo a esta lista!
+// Sempre que criar um jogo novo, é necessário adicioná-lo a esta lista!
 // O site vai atualizar as contagens e os sorteios automaticamente.
 // ======================================================================
 
@@ -16,7 +16,26 @@ const todosOsJogos =[
     { nome: "Comboio das Letras", icone: "🚂", link: "pt/comboio-letras/", cat: "pt" },
     { nome: "Código Secreto", icone: "🕵️‍♂️", link: "pt/codigo-secreto/", cat: "pt" },
     { nome: "Sílaba Inicial", icone: "🗣️", link: "pt/primeira-silaba/", cat: "pt" },
+	{ nome: "28 Palavras", icone: "📖️", link: "pt/28-palavras/", cat: "pt" },
+	{ nome: "Comboio das Sílabas", icone: "🚆️", link: "pt/comboio-silabas/", cat: "pt" },
+	{ nome: "Memória de Imagens", icone: "🖼️", link: "memoria-imagens", cat: "pt" },
+	{ nome: "Palavras e Imagens", icone: "👀", link: "memoria", cat: "pt" },
+	{ nome: "Ordenar Sílabas", icone: "🖐️", link: "arrastar-silabas", cat: "pt" },
+	{ nome: "Escrever Sílabas", icone: "✏️", link: "escrever-silabas", cat: "pt" },
+	{ nome: "Bolhas de Sabão", icone: "🔵", link: "bolhas", cat: "pt" },
+	{ nome: "Monstro das Sílabas", icone: "👾", link: "monstro-silabas", cat: "pt" },
+	{ nome: "Escrever Palavras", icone: "✒️", link: "escrever-palavras", cat: "pt" },
+	{ nome: "Ditado de Palavras", icone: "🔊", link: "ditado-palavras", cat: "pt" },
+	{ nome: "Sopa de letras", icone: "🔎", link: "sopa-letras", cat: "pt" },
+	{ nome: "Copiar Frases", icone: "💻", link: "copia-frases", cat: "pt" },
+	{ nome: "Lanterna Mágica", icone: "🔦", link: "lanterna-magica", cat: "pt" },
+	{ nome: "Verdadeiro ou Falso", icone: "👍👎", link: "verdadeiro-falso", cat: "pt" },
+	{ nome: "Palavra Flash", icone: "📸", link: "palavra-flash", cat: "pt" },
+	{ nome: "Caixa das Adivinhas", icone: "🎁", link: "caixa-adivinhas", cat: "pt" },
+	{ nome: "Jogo da Forca", icone: "🎈", link: "forca", cat: "pt" },
     
+	
+	
     // ------------------------------------
     // MATEMÁTICA (cat: "mat")
     // ------------------------------------
@@ -25,15 +44,21 @@ const todosOsJogos =[
     { nome: "Camião de Carga", icone: "🚚", link: "mat/camiao-carga/", cat: "mat" },
     { nome: "Sapo Saltitão", icone: "🐸", link: "mat/sapo-saltitao/", cat: "mat" },
     { nome: "Gráficos", icone: "📊", link: "mat/graficos/", cat: "mat" },
-    { nome: "Ligar Quant.", icone: "🔗", link: "mat/ligar-quantidades/", cat: "mat" },
-    { nome: "O Mealheiro", icone: "🐷", link: "mat/mealheiro/", cat: "mat" },
+    { nome: "Ligar Quantidades", icone: "🔗", link: "mat/ligar-quantidades/", cat: "mat" },
+    { nome: "Desafio Final", icone: "🏔️", link: "mat/quiz/", cat: "mat" },
+	{ nome: "Memória de Números", icone: "🤔", link: "memoria-numeros", cat: "mat" },
+	{ nome: "Máquina de Somar", icone: "➕", link: "maquina-somar", cat: "mat" },
+	{ nome: "Máquina de Subtrair", icone: "➖", link: "maquina-subtrair", cat: "mat" },
+	{ nome: "Comboio das Sequências", icone: "🚂", link: "sequencias", cat: "mat" },
+	
+	
     
     // ------------------------------------
     // ESTUDO DO MEIO (cat: "em")
     // ------------------------------------
-    { nome: "O Meu Corpo", icone: "🧍", link: "em/corpo/", cat: "em" },
-    { nome: "Os Ecopontos", icone: "♻️", link: "em/ecopontos/", cat: "em" },
-    { nome: "Estações do Ano", icone: "🍂", link: "em/estacoes/", cat: "em" },
-    { nome: "Sons da Natureza", icone: "👂", link: "em/sons-natureza/", cat: "em" },
-    { nome: "As Profissões", icone: "🧑‍🚒", link: "em/profissoes/", cat: "em" }
+    #{ nome: "O Meu Corpo", icone: "🧍", link: "em/corpo/", cat: "em" },
+    #{ nome: "Os Ecopontos", icone: "♻️", link: "em/ecopontos/", cat: "em" },
+    #{ nome: "Estações do Ano", icone: "🍂", link: "em/estacoes/", cat: "em" },
+    #{ nome: "Sons da Natureza", icone: "👂", link: "em/sons-natureza/", cat: "em" },
+    #{ nome: "As Profissões", icone: "🧑‍🚒", link: "em/profissoes/", cat: "em" }
 ];
