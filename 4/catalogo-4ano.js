acbe2c2de82f67a4477b225fc28ddfa02e96216b
professor-edu <jeduardosoeiro@gmail.com>
2026-04-06 19:43:55 +0100

    Add files via upload

---
+++ 4/catalogo-4ano.js
@@ -0,0 +1,32 @@
+// ======================================================================
+// CATÁLOGO CENTRAL DE JOGOS DO 4º ANO
+// Sempre que criar um jogo novo, é necessário adicioná-lo a esta lista!
+// O site vai atualizar as contagens e os sorteios automaticamente.
+// ======================================================================
+
+const todosOsJogos =[
+    // ------------------------------------
+    // PORTUGUÊS (cat: "pt")
+    // ------------------------------------
+    { nome: "Jogo 1 PT", icone: "⌨️", link: "pt/meu-teclado/", cat: "pt" },
+    
+	{ nome: "Jogo 2 PT", icone: "🅰️", link: "pt/vogais", cat: "pt" },
+    
+	
+	
+    // ------------------------------------
+    // MATEMÁTICA (cat: "mat")
+    // ------------------------------------
+    { nome: "Jogo 1 Mat", icone: "🎲", link: "mat/vamos-contar/", cat: "mat" },
+   
+	{ nome: "Jogo 2 Mat", icone: "🚂", link: "mat/sequencias", cat: "mat" },
+	
+	
+    
+    // ------------------------------------
+    // ESTUDO DO MEIO (cat: "em")
+    // ------------------------------------
+    { nome: "Jogo 1 EM", icone: "♻️", link: "em/reciclagem/", cat: "em" },
+   
+	{ nome: "Jogo 1 EM", icone: "🐾", link: "em/cruzadas-animais/", cat: "em" }
+];
