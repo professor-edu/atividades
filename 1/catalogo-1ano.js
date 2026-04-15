8c4e1f63819987357208c3ac80c48b22c93c37c9
professor-edu <jeduardosoeiro@gmail.com>
2026-04-15 01:36:04 +0100

    15 abril

--- 1/catalogo-1ano.js
+++ 1/catalogo-1ano.js
@@ -6,66 +6,62 @@
 
 const todosOsJogos =[
     // ------------------------------------
-    // PORTUGUÊS (cat: "pt")
+    // PORTUGUÊS (cat: "pt") 
     // ------------------------------------
-    { nome: "O Meu Teclado", icone: "⌨️", link: "pt/meu-teclado/", cat: "pt" },
-    { nome: "Palavra Certa", icone: "👁️", link: "pt/palavra-certa/", cat: "pt" },
-    { nome: "Letra em Falta", icone: "🔤", link: "pt/letra-em-falta/", cat: "pt" },
-    { nome: "Contar Sílabas", icone: "👏", link: "pt/contar-silabas/", cat: "pt" },
-    { nome: "Rimas", icone: "🎶", link: "pt/rimas/", cat: "pt" },
-    { nome: "Comboio das Letras", icone: "🚂", link: "pt/comboio-letras/", cat: "pt" },
-    { nome: "Código Secreto", icone: "🕵️‍♂️", link: "pt/codigo-secreto/", cat: "pt" },
-    { nome: "Sílaba Inicial", icone: "🗣️", link: "pt/primeira-silaba/", cat: "pt" },
-	{ nome: "28 Palavras", icone: "📖️", link: "pt/28-palavras/", cat: "pt" },
-	{ nome: "Comboio das Sílabas", icone: "🚆️", link: "pt/comboio-silabas/", cat: "pt" },
-	{ nome: "Memória de Imagens", icone: "🖼️", link: "pt/memoria-imagens", cat: "pt" },
-	{ nome: "Palavras e Imagens", icone: "👀", link: "pt/memoria", cat: "pt" },
-	{ nome: "Ordenar Sílabas", icone: "🖐️", link: "pt/arrastar-silabas", cat: "pt" },
-	{ nome: "Escrever Sílabas", icone: "✏️", link: "pt/escrever-silabas", cat: "pt" },
-	{ nome: "Bolhas de Sabão", icone: "🔵", link: "pt/bolhas", cat: "pt" },
-	{ nome: "Monstro das Sílabas", icone: "👾", link: "pt/monstro-silabas", cat: "pt" },
-	{ nome: "Escrever Palavras", icone: "✒️", link: "pt/escrever-palavras", cat: "pt" },
-	{ nome: "Ditado de Palavras", icone: "🔊", link: "pt/ditado-palavras", cat: "pt" },
-	{ nome: "Sopa de letras", icone: "🔎", link: "pt/sopa-letras", cat: "pt" },
-	{ nome: "Copiar Frases", icone: "💻", link: "pt/copia-frases", cat: "pt" },
-	{ nome: "Lanterna Mágica", icone: "🔦", link: "pt/lanterna-magica", cat: "pt" },
-	{ nome: "Verdadeiro ou Falso", icone: "👍👎", link: "pt/verdadeiro-falso", cat: "pt" },
-	{ nome: "Palavra Flash", icone: "📸", link: "pt/palavra-flash", cat: "pt" },
-	{ nome: "Caixa das Adivinhas", icone: "🎁", link: "pt/caixa-adivinhas", cat: "pt" },
-	{ nome: "Jogo da Forca", icone: "🎈", link: "pt/forca", cat: "pt" },
-	{ nome: "Puzzle das Sílabas", icone: "🧩", link: "pt/puzzle-silabas", cat: "pt" },
-	{ nome: "Vogais", icone: "🅰️", link: "pt/vogais", cat: "pt" },
-    
-	
-	
-    // ------------------------------------
-    // MATEMÁTICA (cat: "mat")
-    // ------------------------------------
-    { nome: "Vamos Contar!", icone: "🎲", link: "mat/vamos-contar/", cat: "mat" },
-    { nome: "O Detetive", icone: "🕵️‍♂️", link: "mat/detetive/", cat: "mat" },
-    { nome: "Camião de Carga", icone: "🚚", link: "mat/camiao-carga/", cat: "mat" },
-    { nome: "Sapo Saltitão", icone: "🐸", link: "mat/sapo-saltitao/", cat: "mat" },
-    { nome: "Gráficos", icone: "📊", link: "mat/graficos/", cat: "mat" },
-    { nome: "Ligar Quantidades", icone: "🔗", link: "mat/ligar-quantidades/", cat: "mat" },
-    { nome: "Desafio Final", icone: "🏔️", link: "mat/quiz/", cat: "mat" },
-	{ nome: "Memória de Números", icone: "🤔", link: "mat/memoria-numeros", cat: "mat" },
-	{ nome: "Máquina de Somar", icone: "➕", link: "mat/maquina-somar", cat: "mat" },
-	{ nome: "Máquina de Subtrair", icone: "➖", link: "mat/maquina-subtrair", cat: "mat" },
-	{ nome: "Comboio das Sequências", icone: "🚂", link: "mat/sequencias", cat: "mat" },
-	
-	
+    { nome: "O Meu Teclado", icone: "icones/meu-teclado.png", link: "pt/meu-teclado/", cat: "pt" },
+    { nome: "Palavra Certa", icone: "icones/palavra_certa.png", link: "pt/palavra-certa/", cat: "pt" },
+    { nome: "Letra em Falta", icone: "icones/letra_em_falta.png", link: "pt/letra-em-falta/", cat: "pt" },
+    { nome: "Contar Sílabas", icone: "icones/contar_silabas.png", link: "pt/contar-silabas/", cat: "pt" },
+    { nome: "Rimas", icone: "icones/rimas.png", link: "pt/rimas/", cat: "pt" },
+    { nome: "Comboio das Letras", icone: "icones/comboio_letras.png", link: "pt/comboio-letras/", cat: "pt" },
+    { nome: "Código Secreto", icone: "icones/codigo_secreto.png", link: "pt/codigo-secreto/", cat: "pt" },
+    { nome: "Sílaba Inicial", icone: "icones/primeira_silaba.png", link: "pt/primeira-silaba/", cat: "pt" },
+    { nome: "28 Palavras", icone: "📖", link: "pt/28-palavras/", cat: "pt" }, 
+    { nome: "Comboio das Sílabas", icone: "🚆", link: "pt/comboio-silabas/", cat: "pt" }, 
+    { nome: "Memória de Imagens", icone: "icones/memoria_imagens.png", link: "pt/memoria-imagens", cat: "pt" },
+    { nome: "Palavras e Imagens", icone: "icones/memoria.png", link: "pt/memoria", cat: "pt" },
+    { nome: "Ordenar Sílabas", icone: "icones/ordenar_silabas.png", link: "pt/arrastar-silabas", cat: "pt" },
+    { nome: "Escrever Sílabas", icone: "icones/puzzle_silabas.png", link: "pt/escrever-silabas", cat: "pt" },
+    { nome: "Bolhas de Sabão", icone: "icones/bolhas_sabao.png", link: "pt/bolhas", cat: "pt" },
+    { nome: "Monstro das Sílabas", icone: "icones/monstro_silabas.png", link: "pt/monstro-silabas", cat: "pt" },
+    { nome: "Escrever Palavras", icone: "icones/ditado_palavras.png", link: "pt/escrever-palavras", cat: "pt" },
+    { nome: "Ditado de Palavras", icone: "icones/ditado_palavras.png", link: "pt/ditado-palavras", cat: "pt" },
+    { nome: "Sopa de letras", icone: "icones/sopa_letras.png", link: "pt/sopa-letras", cat: "pt" },
+    { nome: "Copiar Frases", icone: "💻", link: "pt/copia-frases", cat: "pt" }, 
+    { nome: "Lanterna Mágica", icone: "icones/lanterna_magica.png", link: "pt/lanterna-magica", cat: "pt" },
+    { nome: "Verdadeiro ou Falso", icone: "icones/verdadeiro_falso.png", link: "pt/verdadeiro-falso", cat: "pt" },
+    { nome: "Palavra Flash", icone: "icones/palavra_flash.png", link: "pt/palavra-flash", cat: "pt" },
+    { nome: "Caixa das Adivinhas", icone: "icones/caixa_adivinhas.png", link: "pt/caixa-adivinhas", cat: "pt" },
+    { nome: "Jogo da Forca", icone: "icones/jogo_forca.png", link: "pt/forca", cat: "pt" },
+    { nome: "Puzzle das Sílabas", icone: "icones/puzzle_silabas.png", link: "pt/puzzle-silabas", cat: "pt" },
+    { nome: "Vogais", icone: "icones/vogais.png", link: "pt/vogais", cat: "pt" },
+    { nome: "Cruzadas de Animais", icone: "icones/cruzadas_animais.png", link: "pt/cruzadas-animais", cat: "pt" },
     
     // ------------------------------------
-    // ESTUDO DO MEIO (cat: "em")
+    // MATEMÁTICA (cat: "mat") 
     // ------------------------------------
-    { nome: "Reciclagem", icone: "♻️", link: "em/reciclagem/", cat: "em" },
-    { nome: "Profissões", icone: "🧑‍🚒", link: "em/profissoes/", cat: "em" },
-	{ nome: "Roupas e Estações", icone: "⛅", link: "em/roupas-estacoes/", cat: "em" },
-	{ nome: "Memória do Corpo", icone: "🧠", link: "em/corpo/", cat: "em" },
-	{ nome: "Memória Saudável", icone: "💪", link: "em/vida-saudavel/", cat: "em" },
-	{ nome: "Memória do Vestuário", icone: "👔", link: "em/vestuario/", cat: "em" },
-	{ nome: "Memória dos Alimentos", icone: "🍎", link: "em/alimentos/", cat: "em" },
-	{ nome: "Memória dos Animais", icone: "🐱", link: "em/animais/", cat: "em" },
-	{ nome: "Memória do Tempo", icone: "☀️", link: "em/tempo/", cat: "em" },
-	{ nome: "Cruzadas de Animais", icone: "🐾", link: "em/cruzadas-animais/", cat: "em" }
+    { nome: "Vamos Contar!", icone: "icones/vamos_contar.png", link: "mat/vamos-contar/", cat: "mat" },
+    { nome: "O Detetive", icone: "icones/detetive.png", link: "mat/detetive/", cat: "mat" },
+    { nome: "Camião de Carga", icone: "🚚", link: "mat/camiao-carga/", cat: "mat" }, 
+    { nome: "Sapo Saltitão", icone: "🐸", link: "mat/sapo-saltitao/", cat: "mat" }, 
+    { nome: "Gráficos", icone: "icones/graficos.png", link: "mat/graficos/", cat: "mat" },
+    { nome: "Ligar Quantidades", icone: "icones/ligar_quantidades.png", link: "mat/ligar-quantidades/", cat: "mat" },
+    { nome: "Desafio Final", icone: "🏔️", link: "mat/quiz/", cat: "mat" }, 
+    { nome: "Memória de Números", icone: "icones/memoria_numeros.png", link: "mat/memoria-numeros", cat: "mat" },
+    { nome: "Máquina de Somar", icone: "icones/maquina_somar.png", link: "mat/maquina-somar", cat: "mat" },
+    { nome: "Máquina de Subtrair", icone: "icones/maquina_subtrair.png", link: "mat/maquina-subtrair", cat: "mat" },
+    { nome: "Comboio das Sequências", icone: "icones/comboio_sequencias.png", link: "mat/sequencias", cat: "mat" },
+    
+    // ------------------------------------
+    // ESTUDO DO MEIO (cat: "em") 
+    // ------------------------------------
+    { nome: "Reciclagem", icone: "♻️", link: "em/reciclagem/", cat: "em" }, 
+    { nome: "Profissões", icone: "🧑‍🚒", link: "em/profissoes/", cat: "em" }, 
+    { nome: "Roupas e Estações", icone: "⛅", link: "em/roupas-estacoes/", cat: "em" },
+    { nome: "Memória do Corpo", icone: "icones/memoria_corpo.png", link: "em/corpo/", cat: "em" },
+    { nome: "Memória Saudável", icone: "icones/memoria_saudavel.png", link: "em/vida-saudavel/", cat: "em" },
+    { nome: "Memória do Vestuário", icone: "icones/memoria_vestuario.png", link: "em/vestuario/", cat: "em" },
+    { nome: "Memória dos Alimentos", icone: "icones/memoria_alimentos.png", link: "em/alimentos/", cat: "em" },
+    { nome: "Memória dos Animais", icone: "icones/memoria_animais.png", link: "em/animais/", cat: "em" },
+    { nome: "Memória do Tempo", icone: "icones/memoria_tempo.png", link: "em/tempo/", cat: "em" }
 ];
