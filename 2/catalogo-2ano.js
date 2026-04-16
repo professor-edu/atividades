3597dba40eff643be962d4beaafb5fd3c7d750f7
professor-edu <jeduardosoeiro@gmail.com>
2026-04-16 22:14:40 +0100

    sopa de letras

--- 2/catalogo-2ano.js
+++ 2/catalogo-2ano.js
@@ -21,6 +21,7 @@ const todosOsJogos =[
 	{ nome: "Missão dos Sons", icone: "icones/missao_sons.png", link: "pt/missao-sons", cat: "pt" },
 	{ nome: "Palavras Cruzadas", icone: "icones/cruzadas.png", link: "pt/cruzadas", cat: "pt" },
 	{ nome: "Cruzadas de Animais", icone: "icones/cruzadas_animais.png", link: "pt/cruzadas-animais/", cat: "pt" },
+	{ nome: "Sopa de Letras", icone: "icones/sopa_letras.png", link: "pt/sopa-letras/", cat: "pt" },
 	
 	
     // ------------------------------------
