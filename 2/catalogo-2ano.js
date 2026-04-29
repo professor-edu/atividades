6e19cdc4885afae5aca2b1e47cff84a982424c12
professor-edu <jeduardosoeiro@gmail.com>
2026-04-29 10:30:23 +0100

    Update catalogo-2ano.js

--- 2/catalogo-2ano.js
+++ 2/catalogo-2ano.js
@@ -20,7 +20,7 @@ const todosOsJogos =[
 	{ nome: "Letras Mágicas", icone: "icones/letras_magicas.png", link: "pt/letras-magicas", cat: "pt" },
 	{ nome: "Aventura dos Sons", icone: "icones/aventura_sons.png", link: "pt/aventura-sons", cat: "pt" },
 	{ nome: "Missão dos Sons", icone: "icones/missao_sons.png", link: "pt/missao-sons", cat: "pt" },
-	{ nome: "Ditado de Frsses", icone: "icones/ditado_frases.png", link: "pt/ditado-frases", cat: "pt" },
+	{ nome: "Ditado de Frases", icone: "icones/ditado_frases.png", link: "pt/ditado-frases", cat: "pt" },
 	{ nome: "Palavras Cruzadas", icone: "icones/palavras_cruzadas.png", link: "pt/cruzadas", cat: "pt" },
 	{ nome: "Cruzadas de Animais", icone: "icones/cruzadas_animais.png", link: "pt/cruzadas-animais/", cat: "pt" },
 	{ nome: "Sopa de Letras", icone: "icones/sopa_letras.png", link: "pt/sopa-letras/", cat: "pt" },
